636038fe06a83b83cf343bd25ca706d9665b2c60
momocalderon <51138681+momocalderon@users.noreply.github.com>
2020-06-09 14:40:46 -0500

    carga de diretorios para owl y lightbox
    No olvidar cambiar dirección de imagenes a img

---
+++ js/scripts/scripts.js
@@ -0,0 +1,30 @@
+//console.log('Funciona')
+
+//var nombre= true;
+//var edad=75;
+//console.log(nombre);
+//console.log(edad);
+
+let nombre='Leo';
+
+console.log(nombre);
+
+let banda="metallica",
+    canción="enter sandman";
+
+    let cat= banda + " : "+canción;
+    console.log(cat)
+    console.log(banda.length);
+
+    cat=cat.concat(" "+ "y es genial");
+
+    console.log(cat);
+
+    let separadas =cat.split(" ");
+    console.log(separadas);
+
+
+
+    let incluir= cat.includes("genial");
+    console.log(incluir);
+    alert(Hola)
